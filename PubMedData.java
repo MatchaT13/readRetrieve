7f97c66d43a0feaefd2b04f0704afe76900ef216
MatchaT13 <brian13@my.yorku.ca>
2026-02-04 15:09:11 -0500

    Add files via upload

---
+++ PubMedData.java
@@ -0,0 +1,11 @@
+public class PubMedData {
+
+    public String pmid;
+    public String title;
+
+    public PubMedData(String pmid, String title){
+        this.pmid = pmid;
+        this.title = title;
+    }
+    
+}
